7d5cff28c3b9523eaf5944fa00fba4d32a5ea3d2
Brian Sokol <brian.sokol@slalom.com>
2015-10-12 18:28:40 -0500

    Fix to get tests working, though not yet passing.

--- test/paginatify.test.jsx
+++ test/paginatify.test.jsx
@@ -1,43 +1,50 @@
-import React from 'react';
+import React from 'react/addons';
 import {expect} from 'chai';
-import Paginatify from '../js/paginatify';
+import Paginatify from '../src/js/paginatify';
 
 describe('paginatify', function () {
 
   describe('getPageLinks', function () {
 
     it('should return an empty array given 0 pages', function () {
-      let paginatify = new Paginatify({page: 1, pages: 0});
+      let props = Object.assign(Paginatify.defaultProps, {page: 1, pages: 0, rawOutput: true});
+      let paginatify = new Paginatify(props);
       expect(paginatify.getPageLinks()).to.eql([]);
     });
 
     it('should return an empty array given negative pages', function () {
-      let paginatify = new Paginatify({page: 1, pages: -1});
+      let props = Object.assign(Paginatify.defaultProps, {page: 1, pages: -1, rawOutput: true});
+      let paginatify = new Paginatify(props);
       expect(paginatify.getPageLinks()).to.eql([]);
     });
 
     it('should return an array of 1 given 1 page', function () {
-      let paginatify = new Paginatify({page: 1, pages: 1});
+      let props = Object.assign(Paginatify.defaultProps, {page: 1, pages: 1, rawOutput: true});
+      let paginatify = new Paginatify(props);
       expect(paginatify.getPageLinks()).to.eql([1]);
     });
 
     it('should return an array of numbers given less than 8 pages', function () {
-      let paginatify = new Paginatify({page: 1, pages: 7});
+      let props = Object.assign(Paginatify.defaultProps, {page: 1, pages: 7, rawOutput: true});
+      let paginatify = new Paginatify(props);
       expect(paginatify.getPageLinks()).to.eql([1, 2, 3, 4, 5, 6, 7]);
     });
 
-    it('should return an array of numbers given more than 7 pages with a truncation before the end if he page is less than 5', function () {
-      let paginatify = new Paginatify({page: 3, pages: 10});
+    it('should return an array of numbers given more than 7 pages with a truncation before the end if the page is less than 5', function () {
+      let props = Object.assign(Paginatify.defaultProps, {page: 3, pages: 10, rawOutput: true});
+      let paginatify = new Paginatify(props);
       expect(paginatify.getPageLinks()).to.eql([1, 2, 3, 4, 5, '…', 10]);
     });
 
     it('should return an array of numbers given more than 7 pages with a truncation after 1 if he page is less than 5 from the end', function () {
-      let paginatify = new Paginatify({page: 8, pages: 10});
+      let props = Object.assign(Paginatify.defaultProps, {page: 8, pages: 10, rawOutput: true});
+      let paginatify = new Paginatify(props);
       expect(paginatify.getPageLinks()).to.eql([1, '…', 6, 7, 8, 9, 10]);
     });
 
     it('should return an array of numbers given more than 7 pages with a truncation at the beginning and end if the page is more than 5 but less than 5 from the end', function () {
-      let paginatify = new Paginatify({page: 6, pages: 10});
+      let props = Object.assign(Paginatify.defaultProps, {page: 6, pages: 10, rawOutput: true});
+      let paginatify = new Paginatify(props);
       expect(paginatify.getPageLinks()).to.eql([1, '…', 4, 5, 6, 7, 8, '…', 10]);
     });
   });
