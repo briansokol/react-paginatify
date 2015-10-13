3587a5e036411981ac0cab1886571f7a1128200e
Brian Sokol <bri@nsokol.net>
2015-10-12 22:52:21 -0500

    Condense tests.

--- test/paginatify.test.jsx
+++ test/paginatify.test.jsx
@@ -1,51 +1,49 @@
-import React from 'react/addons';
+//import React from 'react/addons';
 import {expect} from 'chai';
-import Paginatify from '../src/js/paginatify';
+import Paginatify from '../dist/js/react-paginatify';
 
 describe('paginatify', function () {
 
-  describe('getPageLinks', function () {
+  describe('getPageLinks with innerPadding: 1, outerPadding: 1', function () {
 
-    it('should return an empty array given 0 pages', function () {
-      let props = Object.assign(Paginatify.defaultProps, {page: 1, pages: 0, rawOutput: true});
-      let paginatify = new Paginatify(props);
-      expect(paginatify.getPageLinks()).to.eql([]);
-    });
+    var defaultProps       = Paginatify.defaultProps;
+    defaultProps.rawOutput = true;
 
-    it('should return an empty array given negative pages', function () {
-      let props = Object.assign(Paginatify.defaultProps, {page: 1, pages: -1, rawOutput: true});
-      let paginatify = new Paginatify(props);
-      expect(paginatify.getPageLinks()).to.eql([]);
-    });
+    var tests = [
+      {
+        props: Object.assign({}, defaultProps, {page: 1, pages: 0}),
+        expected: []
+      },
+      {
+        props: Object.assign({}, defaultProps, {page: 1, pages: -1}),
+        expected: []
+      }, {
+        props: Object.assign({}, defaultProps, {page: 1, pages: 1}),
+        expected: [1]
+      },
+      {
+        props: Object.assign({}, defaultProps, {page: 1, pages: 7}),
+        expected: [1, 2, '…', 7]
+      },
+      {
+        props: Object.assign({}, defaultProps, {page: 3, pages: 10}),
+        expected: [1, 2, 3, 4, '…', 10]
+      },
+      {
+        props: Object.assign({}, defaultProps, {page: 8, pages: 10}),
+        expected: [1, '…', 7, 8, 9, 10]
+      },
+      {
+        props: Object.assign({}, defaultProps, {page: 6, pages: 10}),
+        expected: [1, '…', 5, 6, 7, '…', 10]
+      }
+    ];
 
-    it('should return an array of 1 given 1 page', function () {
-      let props = Object.assign(Paginatify.defaultProps, {page: 1, pages: 1, rawOutput: true});
-      let paginatify = new Paginatify(props);
-      expect(paginatify.getPageLinks()).to.eql([1]);
-    });
-
-    it('should return an array of numbers given less than 8 pages', function () {
-      let props = Object.assign(Paginatify.defaultProps, {page: 1, pages: 7, rawOutput: true});
-      let paginatify = new Paginatify(props);
-      expect(paginatify.getPageLinks()).to.eql([1, 2, 3, 4, 5, 6, 7]);
-    });
-
-    it('should return an array of numbers given more than 7 pages with a truncation before the end if the page is less than 5', function () {
-      let props = Object.assign(Paginatify.defaultProps, {page: 3, pages: 10, rawOutput: true});
-      let paginatify = new Paginatify(props);
-      expect(paginatify.getPageLinks()).to.eql([1, 2, 3, 4, 5, '…', 10]);
-    });
-
-    it('should return an array of numbers given more than 7 pages with a truncation after 1 if he page is less than 5 from the end', function () {
-      let props = Object.assign(Paginatify.defaultProps, {page: 8, pages: 10, rawOutput: true});
-      let paginatify = new Paginatify(props);
-      expect(paginatify.getPageLinks()).to.eql([1, '…', 6, 7, 8, 9, 10]);
-    });
-
-    it('should return an array of numbers given more than 7 pages with a truncation at the beginning and end if the page is more than 5 but less than 5 from the end', function () {
-      let props = Object.assign(Paginatify.defaultProps, {page: 6, pages: 10, rawOutput: true});
-      let paginatify = new Paginatify(props);
-      expect(paginatify.getPageLinks()).to.eql([1, '…', 4, 5, 6, 7, 8, '…', 10]);
+    tests.forEach(function (test) {
+      it('produces pagination for page: ' + test.props.page + ', pages: ' + test.props.pages, function () {
+        let paginatify = new Paginatify(test.props);
+        expect(paginatify.getPageLinks()).to.eql(test.expected);
+      });
     });
   });
 
